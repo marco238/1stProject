5d83e92397397c9c765566167240864034cf843f
Marcos Monzon <marco238cuba@gmail.com>
2017-11-30 18:32:35 +0100

    nothing new

--- js/canvas.js
+++ js/canvas.js
@@ -1,4 +1,28 @@
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+window.onload = function() {
+  var game = new Game("canvasId");
+
+  document.getElementById("start-button").onclick = function() {
+    game.startGame();
+  };
+
+  function Game(canvasId) {
+    this.canvas = document.getElementById(canvasId);
+    this.ctx = this.canvas.getContext('2d');
+    this.canvas.width = window.innerWidth - 5;
+    this.canvas.height = window.innerHeight - 5.1;
+    this.map = new Map('canvasId');
+  }
+
+  Game.prototype.clear = function() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  };
+
+  Game.prototype.startGame = function() {
+    this.clear();
+    this.intervalId = setInterval(function() {
+      this.map.draw();
+    }.bind(this), 1000 / 60);
+  };
+
+
+};
--- js/road.js
+++ js/road.js
@@ -1,12 +1,30 @@
-function Car(canvas) {
+function Map(canvas) {
   this.canvas = document.getElementById(canvas);
   this.ctx = this.canvas.getContext('2d');
   this.x = 0;
   this.y = 0;
   this.isReady = false;
   this.sprite = new Image();
-  this.sprite.src = "http://spiralforums.biz/uploads/post-53-1139607373.jpg";
+  this.sprite.src = 'img/map.jpg';
   this.sprite.onload = (function() {
     this.isReady = true;
   }).bind(this);
 }
+
+Map.prototype.draw = function() {
+  if (this.isReady) {
+    if (this.x < -this.canvas.width) {
+        this.x = 0;
+      }
+
+      if (this.x < 0) {
+        this.ctx.drawImage(this.sprite, this.sprite.width - this.x, this.y, this.canvas.width, this.canvas.height);
+      } else if (this.x - this.sprite.width > 0) {
+        ctx.drawImage(this.sprite, this.sprite.width * 2 - this.x, this.y, this.canvas.width, this.canvas.height);
+      }
+
+    this.ctx.drawImage(this.sprite, this.x, this.y, this.canvas.width, this.canvas.height);
+
+    this.x -= 0.5;
+  }
+};
